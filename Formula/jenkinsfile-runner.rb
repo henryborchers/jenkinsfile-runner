4ae68af1f55de7adeee5625c8b6fdfdfb4cc5996
Henry Borchers <henryborchers@yahoo.com>
2021-09-24 12:22:07 -0500

    Update jenkinsfile-runner.rb

--- Formula/jenkinsfile-runner.rb
+++ Formula/jenkinsfile-runner.rb
@@ -4,7 +4,7 @@ class JenkinsfileRunner < Formula
     head "https://github.com/jenkinsci/jenkinsfile-runner.git"
 
     bottle :unneeded
-    depends_on :"openjdk"
+    depends_on "openjdk"
     depends_on "maven"
 
     def install
