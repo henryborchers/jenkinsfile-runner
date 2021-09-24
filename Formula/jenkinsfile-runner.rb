4cc9bd1ba5662f876125f4f7a24ea567b2fcddce
Henry Borchers <henryborchers@yahoo.com>
2021-09-24 12:53:55 -0500

    Update jenkinsfile-runner.rb

--- Formula/jenkinsfile-runner.rb
+++ Formula/jenkinsfile-runner.rb
@@ -1,7 +1,7 @@
 class JenkinsfileRunner < Formula
     desc "A command line tool to run Jenkinsfiles"
-    homepage "https://github.com/henryborchers/jenkinsfile-runner"
-    head "https://github.com/jenkinsci/jenkinsfile-runner.git", tag: "1.0-beta-30"
+    homepage "https://github.com/jenkinsci/jenkinsfile-runner"
+    head "https://github.com/henryborchers/jenkinsfile-runner.git", tag: "1.0-beta-30"
 
     bottle :unneeded
     depends_on "openjdk@11"
